98acfdf1b8dc8a3294ad71042d2c0233da4930cf
thierrywo <109875@st-maartenscollege.nl>
2020-01-07 09:45:58 +0100

    Nieuwe tamplate

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -1,34 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using SchoolTemplate.Database;
 using SchoolTemplate.Models;
 
 namespace SchoolTemplate.Controllers
 {
   public class HomeController : Controller
   {
+    // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
+    string connectionString = "Server=172.16.160.21;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
+
     public IActionResult Index()
     {
-            //string connectionString = "Server=172.16.160.21;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
-            //using (MySqlConnection conn = new MySqlConnection(connectionString))
-            //{
-            //  conn.Open();
-            //  MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+      List<Product> products = new List<Product>();
+      // uncomment deze regel om producten uit je database toe te voegen
+      // products = GetProducts();
 
-            //  using (var reader = cmd.ExecuteReader())
-            //  {
-            //    while (reader.Read())
-            //    {
+      return View(products);
+    }
 
-            //      int Id = Convert.ToInt32(reader["Id"]);
-            //      string Name = reader["Naam"].ToString();
-            //    }
-            //  }
+    private List<Product> GetProducts()
+    {
+      List<Product> products = new List<Product>();
 
-            //}
+      using (MySqlConnection conn = new MySqlConnection(connectionString))
+      {
+        conn.Open();
+        MySqlCommand cmd = new MySqlCommand("select * from product", conn);
 
-            return View();
+        using (var reader = cmd.ExecuteReader())
+        {
+          while (reader.Read())
+          {
+            int Id = Convert.ToInt32(reader["Id"]);
+            string Naam = reader["Naam"].ToString();
+            float Calorieen = float.Parse(reader["calorieen"].ToString());
+            string Formaat = reader["Formaat"].ToString();
+            int Gewicht = Convert.ToInt32(reader["Gewicht"].ToString());
+            decimal Prijs = Decimal.Parse(reader["Prijs"].ToString());
+
+            Product p = new Product
+            {
+              Id = Convert.ToInt32(reader["Id"]),
+              Naam = reader["Naam"].ToString(),
+              Calorieen = float.Parse(reader["calorieen"].ToString()),
+              Formaat = reader["Formaat"].ToString(),
+              Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
+              Prijs = Decimal.Parse(reader["Prijs"].ToString())
+            };
+            products.Add(p);
+          }
+        }
+      }
+
+      return products;
     }
 
     public IActionResult Privacy()
---
+++ SchoolTemplate/Database/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SchoolTemplate.Database
+{
+  public class Product
+  {
+    public int Id { get; set; }
+    
+    public string Naam { get; set; }
+
+    public string Formaat { get; set; }    
+
+    /// <summary>
+    /// Gebruik altijd decimal voor geldzaken. Dit doe je om te voorkomen dat er afrondingsfouten optreden
+    /// </summary>
+    public Decimal Prijs { get; set; }
+
+    public int Gewicht { get; set; }
+
+    public float Calorieen { get; set; }
+
+  }
+}
