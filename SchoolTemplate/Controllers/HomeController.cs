657d25eb123b3065ad941db687dfa1903289b6d7
Ashwin Jadoe <110195@st-maartenscollege.nl>
2020-03-09 15:04:05 +0100

    commit voor de pull

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -8,43 +8,70 @@ using SchoolTemplate.Models;
 
 namespace SchoolTemplate.Controllers
 {
-  public class HomeController : Controller
-  {
-    // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
-
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-      List<Product> festivals = new List<Product>(); 
+        // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
+        string connectionString = "Server=172.16.160.21;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
+
+        public IActionResult Index()
+        {
+            List<Product> festivals = new List<Product>();
             // uncomment deze regel om producten uit je database toe te voegen
 
-      return View(festivals);
-    }
-
-
-
-    public IActionResult Privacy()
-    {
-      return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-    
-
-    public IActionResult ShowAll()
-    {
-      return View();
+            return View(festivals);
         }
 
-    [Route("festivals/{id}")]
-public IActionResult Festival(string id)
+
+
+        public IActionResult Privacy()
         {
-            var model = GetFestival(id);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult ShowAll()
+        {
+            return View();
+        }
+
+        [Route("festivals/{id}")]
+        public IActionResult Festivals(string id)
+        {
+            var model = GetFestivals(id);
             return View(model);
         }
+
+        private Festival GetFestivals(string id)
+        {
+            List<Festival> festivals = new List<Festival>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"select * from product where id = {id}", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Festival p = new Festival
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Naam = reader["Naam"].ToString(),
+                            Beschrijving = reader["beschrijving"].ToString()
+                        };
+                        festivals.Add(p);
+                    }
+                }
+            }
+            return festivals[0];
+        }
+
     }
 }
---
+++ SchoolTemplate/Database/Festival.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SchoolTemplate.Database
+{
+  public class Festival
+  {
+    public int Id { get; set; }
+    
+    public string Naam { get; set; }
+
+    public string Beschrijving { get; set; }    
+
+    /// <summary>
+    /// Gebruik altijd decimal voor geldzaken. Dit doe je om te voorkomen dat er afrondingsfouten optreden
+    /// </summary>
+    public Decimal Prijs { get; set; }
+
+    public int Gewicht { get; set; }
+
+    public float Calorieen { get; set; }
+
+  }
+}
