3847bd54f18157fe475e7c8c2be297e1cc8e0339
Ashwin Jadoe <110195@st-maartenscollege.nl>
2020-09-11 08:39:16 +0200

    commit voor de push

---
+++ SchoolTemplate/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolTemplate.Models
+{
+    public class Personmodel
+    {
+        public string Voornaam { get; set; }
+    
+        public string Achternaam { get; set; }
+    }
+}
