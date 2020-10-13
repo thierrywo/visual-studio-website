418d4b9133d5a916e5863ec9bb62b90eee147469
Thierry <Tdwolffenbuttel@gmail.com>
2020-10-13 23:08:00 +0200

    home page

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
+    string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
 
     public IActionResult Index()
     {
