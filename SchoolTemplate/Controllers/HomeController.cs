6a955ebe9c5426ec5a30c577a00be3a425c66b0f
Ashwin Jadoe <110195@st-maartenscollege.nl>
2020-10-16 09:17:22 +0200

    database koppelen opzet meneer logtenberg

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.162.21 ;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
+    string connectionString = "Server=172.16.160.21 ;Port=3306;Database=109875;Uid=109875;Pwd=SprOmyro;";
 
     public IActionResult Index()
     {
@@ -127,6 +127,7 @@ namespace SchoolTemplate.Controllers
     public IActionResult Festival(string id)
     {
       var model = GetFestival(id);
+      ViewData["prijzen"] = GetFestivals();
 
       return View(model);
     }
